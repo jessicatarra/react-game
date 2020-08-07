e2387d63789a8717930b61d3a0449834b93fe32c
Jessica Tarra <jessictarra@gmail.com>
2020-08-07 10:10:51 -0300

    Updates

---
+++ precache-manifest.fc4ca96ada8196e4be2c681aae820867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727005dc49b224e835dd4fb777d1f0dd",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "ccca55ca4a0dbe2a03a5",
+    "url": "/react-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2230e218793a9686f434",
+    "url": "/react-game/static/js/2.32541c8a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-game/static/js/2.32541c8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccca55ca4a0dbe2a03a5",
+    "url": "/react-game/static/js/main.262be60b.chunk.js"
+  },
+  {
+    "revision": "97bb4915e77e8e10ea2d",
+    "url": "/react-game/static/js/runtime-main.5660c68a.js"
+  }
+]);
